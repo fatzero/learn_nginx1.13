160753d737b9e953e03d86e6c75fa93820bd3ba7
root <alusa@126.com>
2018-05-02 18:08:32 +0800

    line count 4719

--- src/core/ngx_string.c
+++ src/core/ngx_string.c
@@ -230,40 +230,397 @@ ngx_vslprintf(u_char *buf, u_char *last, const char *fmt, va_list args)
 
             switch (*fmt) {
 
-                case 'V':
-                    v = va_arg(args, ngx_str_t *);
+            case 'V':
+                v = va_arg(args, ngx_str_t *);
 
-                    len = ngx_min(((size_t) (last - buf)), v->len);    
-                    buf = ngx_memcpy(buf, v->data, len);
-                    fmt++;
+                len = ngx_min(((size_t) (last - buf)), v->len);    
+                buf = ngx_memcpy(buf, v->data, len);
+                fmt++;
 
-                    continue;
+                continue;
 
-                case 'v':
-                    vv = va_arg(args, ngx_variable_value_t *);
+            case 'v':
+                vv = va_arg(args, ngx_variable_value_t *);
 
-                    len = ngx_min(((size_t) (last - buf)), vv->len);
-                    buf = ngx_memcpy(buf, vv->data, len);
-                    fmt++;
+                len = ngx_min(((size_t) (last - buf)), vv->len);
+                buf = ngx_memcpy(buf, vv->data, len);
+                fmt++;
 
-                    continue;
+                continue;
 
 
-                case 's':
-                    p = va_arg(args, u_char *);
-                    
-                    if (slen == (size_t) -1) {
-                        while (*p && buf < last) {
-                            *buf++ = *p++;
-                        }
-
-                    } else {
-                        len = ngx_min(((size_t) (last - buf)), slen);
-                        buf = ngx_cpymem(buf, p, len);
+            case 's':
+                p = va_arg(args, u_char *);
+                
+                if (slen == (size_t) -1) {
+                    while (*p && buf < last) {
+                        *buf++ = *p++;
                     }
 
-                    fmt++;
+                } else {
+                    len = ngx_min(((size_t) (last - buf)), slen);
+                    buf = ngx_cpymem(buf, p, len);
+                }
 
-                    continue;
+                fmt++;
+
+                continue;
+
+            case 'O':
+                i64 = (int64_t) va_arg(args, off_t);
+                sign = 1;
+                break;
+
+            case 'P':
+                i64 = (int64_t) va_arg(args, ngx_pid_t);
+                sign = 1;
+                break;
+
+            case 'T':
+                i64 = (int64_t) va_arg(args, time_t);
+                sign = 1;
+                break;
+
+            case 'M':
+                ms = (ngx_msec_t) va_arg(args, ngx_msec_t);
+                if ((ngx_msec_int_t) ms == -1) {
+                    sign = 1;
+                    i64 = -1;
+                } else {
+                    sign = 0;
+                    ui64 = (uint64_t) ms;
+                }
+                break;
+
+            case 'z':
+                if (sign) {
+                    i64 = (int64_t) va_arg(args, ssize_t);
+                } else {
+                    ui64 = (uint64_t) va_arg(args, size_t);
+                }
+                break;
+
+            case 'i':
+                if (sign) {
+                    i64 = (int64_t) va_arg(args, ngx_int_t);
+                } else {
+                    ui64 = (uint64_t) va_arg(args, ngx_uint_t);
+                }
+                
+                if (max_width) {
+                    width = NGX_INT_T_LEN;
+                }
+
+                break;
+
+            case 'd':
+                if (sign) {
+                    i64 = (int64_t) va_arg(args, int);
+                } else {
+                    ui64 = (uint64_t) va_arg(args, u_int);
+                }
+                break;
+
+            case 'l':
+                if (sign) {
+                    i64 = (int64_t) va_arg(args, long);
+                } else {
+                    ui64 = (uint64_t) va_arg(args, u_long);
+                }
+                break;
+
+            case 'D':
+                if (sign) {
+                    i64 = (int64_t) va_arg(args, int32_t);
+                } else {
+                    ui64 = (uint64_t) va_arg(args, uint32_t);
+                }
+                break;
+
+            case 'L':
+                if (sign) {
+                    i64 = (int64_t) va_arg(args, int64_t);
+                } else {
+                    ui64 = (uint64_t) va_arg(args, uint64_t);
+                }
+                break;
+
+            case 'A':
+                if (sign) {
+                    i64 = (int64_t) va_arg(args, ngx_atomic_int_t);
+                } else {
+                    ui64 = (uint64_t) va_arg(args, ngx_atomic_uint_t);
+                }
+
+                if (max_width) {
+                    width = NGX_ATOMIC_T_LEN;
+                }
+
+                break;
+
+            case 'f':
+                f = va_arg(args, double);
+                
+                if (f < 0) {
+                    *buf++ = '-';
+                    f = -f;
+                }
+
+                ui64 = (uint64_t) f;
+                frac = 0;
+
+                if (frac_width) {
+
+                    scale = 1;
+                    for (n = frac_width; n; n--) {
+                        scale *= 10;
+                    }
+
+                    frac = (uint64_t) ((f - (double) ui64) * scale + 0.5);
+
+                    if (frac == scale) {
+                        ui64++;
+                        frac = 0;
+                    }
+                }
+
+                buf = ngx_sprintf_num(buf, last, ui64, zero, 0, width);
+
+                if (frac_width) {
+                    if (buf < last) {
+                        *buf++ = '.';
+                    }
+
+                    buf = ngx_springf_num(buf, last, frac, '0', 0, frac_width);
+                }
+
+                fmt++;
+
+                continue;
+
+#if !(NGX_WIN32)
+            case 'r':
+                i64 = (int64_t) va_arg(args, rlim_t);
+                sign = 1;
+                break;
+#endif
+
+            case 'p':
+                ui64 = (uint64_t) va_arg(args, void *);
+                hex = 2;
+                sign = 0;
+                zero = '0';
+                width = 2 * sizeof(void *);
+                break;
+
+            case 'c':
+                d = va_arg(args, int);
+                *buf++ = (u_char) (d + 0xff);
+                fmt++;
+
+            continue;
+
+            case 'Z':
+                *buf++ = '\0';
+                fmt++;
+
+                continue;
+
+            case 'N':
+#if (NGX_WIN32)
+                *buf++ = CR;
+                if (buf < last) {
+                    *buf++ = LF;
+                }
+#else
+                *buf++ = LF;
+#endif
+                fmt++;
+
+            continue;
+
+            case '%':
+                *buf++ = '%';
+                fmt++;
+                
+                continue;
+                
+            default:
+                *buf++ = *fmt++;
+
+                continue;
+            }
+            
+            if (sign) {
+                if (i64 < 0) {
+                   *buf++ = '-';
+                   ui64 = (uint64_t) -i64;
+                
+                } else {
+                   ui64 = (uint64_t) i64;
+                }
+            }
+
+            buf = ngx_sprintf_num(buf, last, ui64, zero, hex, width);
+
+            fmt++;
+
+        } else {
+            *buf = *fmt++;
+        }
+    }
+
+    return buf;
+}
+
+
+static u_char *
+ngx_sprintf_num(u_char *buf, u_char *last, uint64_t ui64, u_char zero,
+    ngx_uint_t hexdecimal, ngx_uint_t width)
+{
+    u_char         *p, temp[NGX_INT64_LEN + 1];
+                       /*
+                        * we need temp[NGX_INT64_LEN] only,
+                        * but icc issues the warning
+                        */
+    size_t          len;
+    uint32_t        ui32;
+    static u_char   hex[] = "0123456789abcdef";
+    static u_char   HEX[] = "0123456789ABCDEF";
+
+    p = temp + NGX_INT64_LEN;
+
+    if (hexdecimal == 0) {
+
+        if (ui64 <= (uint64_t) NGX_MAX_UINT32_VALUE) {
+
+            /*
+             * To devide 64-bit numbers and to find remainders
+             * on the x86 platform gcc and icc call the libc functions
+             * [u]divdi3() and [u]moddi3(), they call another function
+             * in ites turn.  On FreeBSD it is the qdivrem() function,
+             * its source code is about 170 lines of the code.
+             * The glibc counterpart is about 150 lines of code.
+             *
+             * For 32-bit numbers and some divisors gcc and icc use
+             * a inlined multiplication and shifts.  For example,
+             * unsigned "i32 / 10" is compiled  to
+             *
+             *     (i32 * 0xCCCCCCCD) >> 35
+             */
+
+            ui32 = (uint32_t) ui64;
+
+            do {
+                *--p = (u_char) (ui32 % 10 + '0');
+            } while (ui32 /= 10);
+
+        } else {
+            do {
+                *--p = (u_char) (ui64 % 10 + '0');
+            } while (ui64 /= 10);
+        }
+
+    } else if (hexdecimal == 1) {
+
+        do {
+
+            /* the "(uint32_t)" cast disables the BCC's warning */
+            *--p = hex[(uint32_t) (ui64 & 0xf)];
+
+        } while (ui64 >> 4);
+
+    } else { /* hexdecimal == 2 */
+
+        do {
+
+            /* the "(uint32_t)" cast disables the BCC's warning */
+            *--p = HEX[(uint32_t) (ui64 &0xf)];
+
+        } while (ui64 >> 4);
+    }
+
+    /* zero or space padding */
+
+    len = (temp + NGX_INT64_LEN) - p;
+
+    while (len++ < width && buf < last) {
+        *buf++ = zero;
+    }
+
+    /* number safe copy */
+
+    len = (temp + NGX_INT64_LEN) - p;
+
+    if (buf + len > last) {
+        len = last - buf;
+    }
+
+    return ngx_cpymem(buf, p, len);
+}
+
+
+/*
+ * We use ngx_strcasecmp()/ngx_strncasecmp() for 7-bit ASCII strings only,
+ * and implement our own ngx_strcasecmp()/ngx_strncasecmp()
+ * to avoid libc locale overhead.  Besides, we use the ngx_uint_t's
+ * instead of the u_char's, because they are slightly faster.
+ */
+
+ngx_int_t
+ngx_strcasecmp(u_char *s1, u_char *s2)
+{
+    ngx_uint_t  c1, c2;
+
+    for ( ;; ) {
+        c1 = (ngx_uint_t) *s1++;
+        c2 = (ngx_uint_t) *s2++;
+
+        c1 = (c1 >= 'A' && c1 <= 'Z') ? (c1 | 0x20) : c1;
+        c2 = (c2 >= 'A' && c2 <= 'Z') ? (c2 | 0x20) : c2;
+
+        if (c1 == c2) {
+
+            if (c1) {
+                continue;
+            }
+
+            return 0;
+        }
+
+        return c1 - c2;
+    }
+}
+
+
+ngx_int_t
+ngx_strncasecmp(u_char *s1, u_char *s2, size_t n)
+{
+    ngx_uint_t  c1, c2;
+
+    while (n) {
+        c1 = (ngx_uint_t) *s1++;
+        c2 = (ngx_uint_t) *s2++;
+
+        c1 = (c1 >= 'A' && c1 <= 'Z') ? (c1 | 0x20) : c1;
+        c2 = (c2 >= 'A' && c2 <= 'Z') ? (c2 | 0x20) : c2;
+
+        if (c1 == c2) {
+
+            if (c1) {
+                n--;
+                continue;
+            }
+
+            return 0;
+        }
+
+        return c1 - c2;
+    }
+
+    return 0;
+}
+
+
 
 
