4fca41ee0b3b192b8e4460d77fca6e775284522f
root <alusa@126.com>
2018-05-14 00:54:27 +0800

    line count 5772

---
+++ src/core/ngx_spinlock.c
@@ -0,0 +1,21 @@
+
+/*
+ * Copyright (C) Igor Sysoev
+ * Copyright (C) Nginx, Inc.
+ */
+
+
+#include <ngx_config.h>
+#include <ngx_core.h>
+
+
+void
+ngx_spinlock(ngx_atomic_t *lock, ngx_atomic_int_t value, ngx_uint_t spin)
+{
+
+#if (NGX_HAVE_ATOMIC_OPS)
+
+    ngx_uint_t  i, n;
+
+    for ( ;; ) {
+
