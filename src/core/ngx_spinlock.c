f2e6e03aea32c2b052ea5ced2eb90a75e455dfbc
root <alusa@126.com>
2018-05-14 15:57:13 +0800

    line count 6113

--- src/core/ngx_spinlock.c
+++ src/core/ngx_spinlock.c
@@ -19,3 +19,35 @@ ngx_spinlock(ngx_atomic_t *lock, ngx_atomic_int_t value, ngx_uint_t spin)
 
     for ( ;; ) {
 
+        if (*lock == 0 && ngx_atomic_cmp_set(lock, 0, value)) {
+            return;
+        }
+
+        if (ngx_ncpu > 1) {
+
+            for (n = 1; n < spin; n <<= 1) {
+
+                for (i = 0; i < n; i++) {
+                    ngx_cpu_pause();
+                }
+
+                if (*lock == 0 && ngx_atomic_cmp_set(lock, 0, value)) {
+                    return;
+                }
+            }
+        }
+
+        ngx_sched_yield();
+    }
+
+#else
+
+#if (NGX_THREADS)
+
+#error ngx_spinlock() or ngx_atomic_cmp_set() are not defined !
+
+#endif
+
+#endif
+
+}
