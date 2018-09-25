a89e3067a029ec98a6e8266a1def4ec4ead64521
root <alusa@126.com>
2018-09-26 00:24:09 +0800

    line count 10605

--- src/core/ngx_slab.c
+++ src/core/ngx_slab.c
@@ -448,4 +448,60 @@ ngx_slab_free(ngx_slab_pool_t *pool, void *p)
 }
 
 
+static ngx_slab_page_t *
+ngx_slab_alloc_pages(ngx_slab_pool_t *pool, ngx_uint_t pages)
+{
+    ngx_slab_page_t   *page, *p;
+
+    for (page = pool->free.next; page != &pool->free; page = page->next) {
+
+        if (page->slab >= pages) {
+
+            if (page->slab > pages) {
+                page[page->slab - 1].prev = (uintptr_t) &page[pages];
+
+                page[pages].slab = page->slab - pages;
+                page[pages].next = page->next;
+                page[pages].prev = page->prev;
+
+                p = (ngx_slab_page_t *) page->prev;
+                p->next = &page[pages];
+                page->next->prev = (uintptr_t) &page[pages];
+
+            } else {
+                p = (ngx_slab_page_t *) page->prev;
+                p->next = page->next;
+                page->next->prev = p->prev;
+            }
+
+            page->slab = pages | NGX_SLAB_PAGE_START;
+            page->next = NULL;
+            page->prev = NGX_SLAB_PAGE;
+
+            pool->pfree -= pages;
+
+            if (--pages) {
+                return page;
+            }
+
+            for (p = page + 1; pages; pages--) {
+                p->slab = NGX_SLAB_PAGE_BUSY;
+                p->next = NULL;
+                p->prev = NGX_SLAB_PAGE;
+                p++;
+            }
+
+            return page;
+        }
+    }
+
+    if (pool->log_nomem) {
+        ngx_slab_error(pool, NGX_LOG_CRIT,
+                       "ngx_slab_alloc() failed: no memory");
+    }
+
+    return NULL;
+}
+
+
 
